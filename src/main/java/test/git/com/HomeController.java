1f65aaa53040903260131c2242ac00c18bbec7dc
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2020-06-06 15:02:53 +0900

    쪞~~`

--- src/main/java/test/git/com/HomeController.java
+++ src/main/java/test/git/com/HomeController.java
@@ -26,6 +26,8 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		
+		System.out.println("랄로로로로로");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
